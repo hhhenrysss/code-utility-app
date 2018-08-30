4625dfbe5d2d370bc48a13f289caac9da321167a
hhhenrysss <25966196+hhhenrysss@users.noreply.github.com>
2018-08-29 22:32:28 -0500

    restructured data flow

--- Documentation/js/components/sidebar.jsx
+++ Documentation/js/components/sidebar.jsx
@@ -1,4 +1,6 @@
 const React = require('react');
+const extract = require(path.join(node_path, `Documentation/languages/${redirect.name}/js/extract.js`)).extract;
+const receiver = extract();
 
 class SideBarItemIcons extends React.Component {
     render() {
@@ -15,27 +17,64 @@ class SideBarItemContents extends React.Component {
 class SideBarItem extends React.Component {
     render() {
         return [
-            <li>
-                <SideBarItemIcons type={this.props.type}/>,
+            <li className={'SideBarItem'}>
+                <SideBarItemIcons type={this.props.type}/>
                 <SideBarItemContents name={this.props.name}/>
             </li>
         ]
     }
 }
 
+class SideBarGroup extends React.Component {
+
+
+    expand_group() {
+        if (this.props.if_current_module) {
+            receiver.code.get_all_code_ids(this.state.current_module_name)
+                .then((code_ids) => {
+                    this.props.updating_methods.update_current_module_name(this.props.name);
+                    let promise_array
+                    for (id of code_ids) {
+
+                    }
+                })
+                .catch((error) => {
+                    throw error;
+                });
+        }
+    }
+
+    render() {
+        return
+    }
+}
+
 // todo: add key for each child
 module.exports = class SideBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = (() => {
+            return {
+                active_group_index: 0
+            }
+        })()
+    }
+
     render() {
-        let items = [];
-        items.push(
-            <h3 className={'func_navigation_header'}> Documentation </h3>
+        // todo: finish this drop down list
+        let rendered_items = [];
+        rendered_items.push(
+            <div className={'SideBar_navigation_header'}> Documentation </div>
         );
+
+
         for (name of this.props.module_list) {
-            items.push(
-                <SideBarItem name={name} type={'module'}/>
+            rendered_items.push(
+                <SideBarGroup name={name} if_current_module={ name === this.props.current_module } updating_methods={this.props.updating_methods}/>
             )
         }
-        return <nav className={'func_navigation'}><ul> {items} </ul></nav>
+
+        return <nav className={'SideBar_navigation'}><ul> {rendered_items} </ul></nav>
     }
 };
--- Documentation/js/documentation.jsx
+++ Documentation/js/documentation.jsx
@@ -19,13 +19,31 @@ class Main extends React.Component {
         this.state = (function () {
             return {
                 all_modules: [],
-                current_module: 'Test flight',
+                current_module_name: 'Test flight',
                 current_content: 'xxx',
-                current_function: 'func',
-                is_init_screen: true
+                current_function: ['func']
             }
         })()
     }
+
+    update_current_module_name(module_name_string) {
+        this.setState({
+            current_module_name: module_name_string
+        })
+    }
+
+    update_current_content(complete_doc) {
+        this.setState({
+            current_content: complete_doc
+        })
+    }
+
+    update_current_function(array_of_functions) {
+        this.setState({
+            current_function: array_of_functions
+        })
+    }
+
 
     componentWillMount() {
         receiver.module.get_all_module_names()
@@ -39,9 +57,15 @@ class Main extends React.Component {
 
     render() {
         return [
-            <SideBar module_list={this.state.all_modules}/>,
-            <Article current_module={this.state.current_module} currrent_state={this.state.current_function}/>,
-            <Header current_module={this.state.current_module}/>,
+            <SideBar module_list={this.state.all_modules} current_module={this.state.current_module_name} updating_methods={(() => {
+                return {
+                    update_current_module_name: this.update_current_module_name,
+                    update_current_function: this.update_current_function,
+                    update_current_content: this.update_current_content
+                }
+            })()}/>,
+            <Article current_module={this.state.current_module_name} currrent_state={this.state.current_function}/>,
+            <Header current_module={this.state.current_module_name}/>,
             <Footer/>
         ]
     }
